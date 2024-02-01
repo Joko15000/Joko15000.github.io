3eba358cb40781fba4276a1f085b316080479a93
Joko15000 <153940286+Joko15000@users.noreply.github.com>
2024-02-02 04:11:46 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function calculate() {
-    // Your Gaussian Elimination Back Substitution calculation logic goes here
-
-    // Display the result
-    document.getElementById('result').innerHTML = '<h2>Result:</h2><p>Your result will be displayed here.</p>';
-    
-    // Display tutorial
-    document.querySelector('.tutorial').innerHTML = '<h2>Tutorial:</h2><p>Here goes the step-by-step tutorial on how to interpret the result.</p>';
-}
